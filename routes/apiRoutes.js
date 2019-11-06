c59d662b1680283183f42078951a7f269da7f29f
Kelly Jiang <kllyjiang@gmail.com>
2019-11-06 16:34:11 -0600

    Configuring express routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,31 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    app.get("/api/games", function(req, res) {
+        db.Game.findAll({}).then(games => {
+            res.json(games);
+        });
     });
-  });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    app.get("/api/games/:id", function(req, res) {
+        db.Game.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(game => {
+            res.json(game);
+        });
     });
-  });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    app.post("/api/games", function(req, res) {
+        db.Game.create(req.body).then(function(game) {
+            res.json(game);
+        });
     });
-  });
+
+    app.delete("/api/games/:id", function(req, res) {
+        db.Game.destroy({ where: { id: req.params.id } }).then(function(game) {
+            res.json(game);
+        });
+    });
 };
