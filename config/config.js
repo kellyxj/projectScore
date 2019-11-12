b53d3f5169bccabbb0b35aa4f8beeb55cf9746d6
Kelly Jiang <kllyjiang@gmail.com>
2019-11-11 21:35:49 -0600

    Fixing links to external js and css. Editing config file to use environmental variable

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+module.exports = {
+    development: {
+        username: "root",
+        password: process.env.PASSWORD_DB,
+        database: "gamesDB",
+        host: "localhost",
+        dialect: "mysql"
+    },
+    test: {
+        username: "root",
+        password: null,
+        database: "testdb",
+        host: "localhost",
+        dialect: "mysql",
+        logging: false
+    },
+    production: {
+        use_env_variable: "JAWSDB_URL",
+        dialect: "mysql"
+    }
+};
